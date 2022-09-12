7509d5b3b4246a40e2699aae2e40c8a409f3de8d
RohitPandey1294 <110982022+RohitPandey1294@users.noreply.github.com>
2022-09-12 10:10:24 +0530

    Add files via upload

---
+++ LoginAPI/src/main/java/com/hrportal/main/service/JobDescriptionServiceInterface.java
@@ -0,0 +1,24 @@
+package com.hrportal.main.service;
+
+import java.util.List;
+
+import com.hrportal.main.domain.JobDescription;
+
+public interface JobDescriptionServiceInterface {
+	public List<JobDescription> getallJobDescriptionDetails();
+
+	public List<JobDescription> getJobDescriptionByEmployeeId(String employeeId);
+
+	public boolean addNewJobDescription(JobDescription jobdescription);
+
+	public List<JobDescription> getJobDescriptionByProjectId(String projectId);
+
+	public JobDescription updateRequiredCandidatesByJobId(JobDescription jobDescription);
+
+	public JobDescription updateStatusByJobId(JobDescription jobDescription);
+
+	public JobDescription updateStatusForHrByJobId(JobDescription jobDescription);
+
+	public List<JobDescription> getJobDescriptionForHrByProjectId(String projectId);
+
+}
